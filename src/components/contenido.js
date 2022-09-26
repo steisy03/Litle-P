bb9b860ed995806a2266642eb7c2fea5d8bfb07b
steisy03 <steisycrisostomo@outlook.com>
2022-09-26 16:45:39 -0400

    agregando contenido

--- src/components/contenido.js
+++ src/components/contenido.js
@@ -1,5 +1,7 @@
 import Route from '../libs/route';
+import {sistemaDigestivo} from '../data/contenido';
 
+let contenidos;
 class Contenido extends Route {
     constructor() {
         super("contenido", {
@@ -9,7 +11,23 @@ class Contenido extends Route {
         this.onMountCb = this.whenMounted;
     }
 
-    async whenMounted() {}
+    async whenMounted() {
+        //DECLARAR
+        contenidos = document.getElementById( 'contenidos' );
+        //
+        const url = window.location.search;
+        const urlParams = new URLSearchParams( url );
+        const option = urlParams.get( 'sistema' );
+        console.log(option);
+        //
+        if( option == 'sistema-digestivo' ){
+            contenidos.innerHTML = sistemaDigestivo;
+        } else if ( option == 'sistema-respiratorio' ){
+
+        }else{
+            window.location = '/';
+        }
+    }
 }
 
 const contenido = new Contenido();
