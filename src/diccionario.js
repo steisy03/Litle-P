1dd89944fe9a6f987b492ada92a43c0c3605ff02
steisy03 <steisycrisostomo@outlook.com>
2022-09-30 19:30:43 -0400

    filtro

--- src/diccionario.js
+++ src/diccionario.js
@@ -1,22 +1,38 @@
 import { tablaContenido } from './template/links';
 import {templateDiccionario} from './template/tmpDiccionario';
-const definiciones = require('./data/diccionario.json');;
-let tablaContenidos, template, data;
-
+const definiciones = require('./data/diccionario.json');
+//
+let tablaContenidos, data, buscarDiccionario;
 
 window.onload = function() {
     //DECLARAR
-    tablaContenidos = document.getElementById('contenidos');
-    data = document.getElementById('listaElementos');
-
-    
+    tablaContenidos = document.getElementById( 'contenidos' );
+    data = document.getElementById( 'listaElementos' );
+    buscarDiccionario = document.getElementById( 'buscarDiccionario' );
+    //EVENTOS
+    buscarDiccionario.addEventListener('keyup', filtrar);
+    //
     tablaContenidos.innerHTML = tablaContenido;
-    llenarConceptos();
+    llenarConceptos( definiciones );
 }
 
-function llenarConceptos(){
-    let sp='';
+function filtrar(){
+    let arr = [];
     definiciones.forEach((elemento) =>{
+        arr.push(elemento);
+    })
+    if(buscarDiccionario.value == ""){
+        arr = definiciones;
+    } else {
+        arr = arr.filter( row => row.nombre.toLowerCase().includes(buscarDiccionario.value) ) ;
+        console.log(arr);
+    }
+    llenarConceptos( arr);
+}
+
+function llenarConceptos( arr ){
+    let sp='';
+    arr.forEach((elemento) =>{
         sp += templateDiccionario
         .replace( "{{NOMBRE}}", elemento.nombre )
         .replace( "{{TIPO}}", elemento.tipo )
